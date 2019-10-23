2287404de9bafbdf496d4823b20623b3b354e3ed
o0Maikeru0o <mnickandros@gmail.com>
2019-10-23 17:36:47 -0400

    p2p socket methods created

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -18,5 +18,27 @@ class P2P_Server {
     this.connectToPeers();
     console.log(`Listening for peer to peer connection on port : ${P2P_PORT}`);
   }
+
+  connectSocket(socket) {
+    this.sockets.push(socket);
+    console.log(`Connected to socket ${socket.id}`);
+    this.messageHandler(socket);
+    socket.send(JSON.stringify(this.blockchain));
+  }
+
+  connectToPeers() {
+    peers.forEach((peer) => {
+      const socket = new WebSocket(peer);
+      socket.id = uuidv4();
+      socket.on('open', () => this.connectSocket(socket));
+    });
+  }
+
+  messageHandler(socket) {
+    socket.on('message', (message) => {
+      const data = JSON.parse(message);
+      console.log('data', data);
+    });
+  }
 }
 module.exports = P2P_Server;
